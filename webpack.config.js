eb20453f15f23568da7bc0ebc836135d1892bab6
UncleCode (Hossein Tohidi) <unclecode@kidocode.com>
2019-06-08 13:01:17 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const env = process.env.NODE_ENV
+const path = require('path');
 
 module.exports = env => {
   console.log(`🛠️  running ${env} Mode using ./webpack/webpack.${env}.js 🛠️`);
-  return require(`./webpack/webpack.${env}.js`);
+  let common = {
+    resolve: {
+      alias: {
+        Appomata: path.resolve(__dirname, 'src/modules/Appomata.js'),
+        modules: path.resolve(__dirname, 'src/modules/')
+      }
+    }
+  }
+  return Object.assign(require(`./webpack/webpack.${env}.js`), common);
 };
